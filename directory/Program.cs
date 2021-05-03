933d386873d509afb17f4e650039e91d80490a3e
Elizaveta <liza_fahrut@mail.ru>
2021-05-03 17:45:33 +1000

    new version

--- directory/Program.cs
+++ directory/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Collections;
 using System.IO.Compression;
 
 
@@ -12,17 +11,12 @@ namespace directory
         static void Main(string[] args)
         {
             Console.WriteLine("Введите путь каталога, который необходимо обработать:");
-
-            string mypath = Console.ReadLine();
-            //String mypath = @"C:\Users\Serega\Desktop\1\student-2021-assignment-master\student-2021-assignment-master\src\Utrom's secrets";
-
-            DirectoryInfo droot = new DirectoryInfo(mypath);
-            
+            string path = Console.ReadLine();
+            DirectoryInfo rootdirectory = new DirectoryInfo(path);
             List<DirectoryInfo> directories = new List<DirectoryInfo>();
-            directories.AddRange(droot.GetDirectories("*"));
-            
+            directories.AddRange(rootdirectory.GetDirectories("*"));
             string rootpath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            
+
             string subpath = "dest\\Utrom's secrets";
             DirectoryInfo dirInfo = new DirectoryInfo(rootpath);
             if (!dirInfo.Exists)
@@ -30,99 +24,90 @@ namespace directory
                 dirInfo.Create();
             }
             dirInfo.CreateSubdirectory(subpath);
-            
-            List<FileInfo> files = new List<FileInfo>();
-            files.AddRange(droot.GetFiles("*"));
-            List<FileInfo> filesName = new List<FileInfo>();
 
+            List<FileInfo> files = new List<FileInfo>();
+            files.AddRange(rootdirectory.GetFiles("*"));
             List<string> newpaths = new List<string>();
 
             foreach (FileInfo file in files)
-            {                
-                string obrez = file.FullName.Substring(file.FullName.LastIndexOf(" "), file.FullName.IndexOf(".") - file.FullName.LastIndexOf(" "));
-                         
-                string newpath = file.FullName.Replace(obrez, ""); //obrezat'
-                
-                if (File.Exists(rootpath+"\\" + subpath+"\\"+file.Name.Replace(obrez, "")))
-                {
-                    if (Razmer(file.FullName)>Razmer(rootpath +"\\"+ subpath + "\\" + file.Name.Replace(obrez, "")))//if файл из старой директории больше, чем тот, что в новой
-                    {
-                        File.Copy(file.FullName, rootpath + "\\" + subpath + "\\" + file.Name.Replace(obrez, ""), true);
-                    }
-                }
-                else
-                {
-                    File.Copy(file.FullName, rootpath+"\\" + subpath + "\\" + file.Name.Replace(obrez, ""));
-                }
-                
-            }
-            foreach(DirectoryInfo directoryInfo in directories)
             {
-                string obrez = directoryInfo.FullName.Substring(directoryInfo.FullName.LastIndexOf(" "));
-                string newpath = directoryInfo.FullName.Replace(obrez, "");
-                if (Directory.Exists(rootpath + "\\" + subpath + "\\" + directoryInfo.Name.Replace(obrez, "")))
+                string normalnamefile = file.Name.Substring(0, file.Name.LastIndexOf(' ')) + file.Name.Substring(file.Name.LastIndexOf('.'));
+                string newpath = rootpath + "\\" + subpath + "\\" + normalnamefile;
+                if (File.Exists(newpath))
                 {
-                    if(GetSize(directoryInfo.FullName) > GetSize(rootpath + "\\" + subpath + "\\" + directoryInfo.Name.Replace(obrez, "")))
+                    if (SizeFile(file.FullName) > SizeFile(newpath))//if файл из старой директории больше, чем тот, что в новой
                     {
-                        DirectoryCopy(directoryInfo.FullName, rootpath + "\\" + subpath + "\\" + directoryInfo.Name.Replace(obrez, ""));
+                        File.Copy(file.FullName, newpath, true);
                     }
-                    
                 }
                 else
                 {
-                    DirectoryCopy(directoryInfo.FullName, rootpath + "\\" + subpath + "\\" + directoryInfo.Name.Replace(obrez, ""));
+                    File.Copy(file.FullName, newpath);
                 }
             }
-            ZipFile.CreateFromDirectory(rootpath+"\\" + subpath, rootpath+"\\" + subpath + ".zip");
+            foreach (DirectoryInfo directoryInfo in directories)
+            {
+                string fullnormalnamefolder = rootpath + "\\" + subpath + "\\" + directoryInfo.FullName.Substring(0, directoryInfo.FullName.LastIndexOf(' '));
+
+                if (Directory.Exists(fullnormalnamefolder))
+                {
+                    if (SizeDirectory(directoryInfo.FullName) > SizeDirectory(fullnormalnamefolder))
+                    {
+                        DirectoryCopy(directoryInfo.FullName, fullnormalnamefolder);
+                    }
+                }
+                else
+                {
+                    DirectoryCopy(directoryInfo.FullName, fullnormalnamefolder);
+                }
+            }
+            ZipFile.CreateFromDirectory(rootpath + "\\" + subpath, rootpath + "\\" + subpath + ".zip");
             Console.WriteLine("Каталог обработан");
             Console.ReadKey();
         }
         public static void DirectoryCopy(string sourceDirName, string destDirName)
         {
-            
             DirectoryInfo dir = new DirectoryInfo(sourceDirName);
-
             if (!dir.Exists)
             {
                 throw new DirectoryNotFoundException("Source directory does not exist or could not be found: " + sourceDirName);
             }
             DirectoryInfo[] dirs = dir.GetDirectories();
-     
+
             Directory.CreateDirectory(destDirName);
-            
+
             FileInfo[] files = dir.GetFiles();
             foreach (FileInfo file in files)
             {
                 string tempPath = Path.Combine(destDirName, file.Name);
                 file.CopyTo(tempPath, false);
             }
-                        
-                foreach (DirectoryInfo subdir in dirs)
-                {
-                    string tempPath = Path.Combine(destDirName, subdir.Name);
-                    DirectoryCopy(subdir.FullName, tempPath);
-                }
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string tempPath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, tempPath);
+            }
         }
 
-    static public int GetSize(string path)
+        static public int SizeDirectory(string path)
         {
             int size = 0;
-            List<string> info = new List<string>();
-            info.AddRange(Directory.GetFiles(path));
-            foreach (string file in info)
+            List<string> filenames = new List<string>();
+            filenames.AddRange(Directory.GetFiles(path));
+            foreach (string file in filenames)
             {
-                size += Razmer(file);
+                size += SizeFile(file);
             }
             return size;
         }
-        static public int Razmer(string newpath)
+        static public int SizeFile(string newpath)
         {
             string size = "";
             StreamReader streamReader = new StreamReader(newpath);
             char[] sizetext = streamReader.ReadToEnd().ToCharArray();
             streamReader.Close();
             int i = 0;
-            while (char.IsDigit(sizetext[i]) && i<sizetext.Length)
+            while (char.IsDigit(sizetext[i]) && i < sizetext.Length)
             {
                 size += sizetext[i];
                 i++;
